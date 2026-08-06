4da2aef8df12186b882e01ffb5e54d3a71e9e1fa
Donovan <donovanbueno456@gmail.com>
2026-08-06 16:23:54 -0300

    ajustes no layout e mudanças no banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,10 +4,10 @@ function Banner() {
         <>
         <section className="banner-start">
             <div className='banner-content'>
-                <h1 id="banner-h1">Let Your Home <br/>Be Unique</h1>
-                <p id="banner-p">There are many variations of the passages of<br/> lorem Ipsum fromavailable, majority.</p>
+                <h1 id="banner-h1">Conheça seu mais<br/>novo Desenvolvedor <br/>de Software Multiplataforma</h1>
+                <p id="banner-p">Sou um desenvolvedor de software multiplataforma, especializado em criar <br/>soluções inovadoras e eficientes para diversos ambientes.</p>
                 <br/>
-                <button onClick="" id="banner-btn">Get Started →</button>
+                <button onClick="" id="banner-btn">Mais Informações →</button>
             </div>
         </section>
         </>
