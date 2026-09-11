580ff00d9e6906269c0e76927f8d33131c39acd0
Donovan Bueno <134464142+DevDonov4n@users.noreply.github.com>
2026-09-11 19:31:29 -0300

    feat: add language provider to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from "./context/ThemeProvider";
+import { LanguageProvider } from "./context/LanguageProvider";
 
 // PAGES
-
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 
-
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path='/' element= {<Home />}></Route>
-          <Route path='/about' element= {<About />}></Route>
-          <Route path='/projects' element= {<Projects />}></Route>
-          <Route path='/contact' element= {<Contact />}></Route>
-        </Routes>
-      </Router>
+      <LanguageProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
