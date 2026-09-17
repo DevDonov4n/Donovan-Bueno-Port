efdb6c41c04f5a8299e055f47a5e34b8720cd1d3
Donovan Bueno <134464142+DevDonov4n@users.noreply.github.com>
2026-09-16 21:21:05 -0300

    fix: use uploaded certificate PDF from assets

--- src/components/Certificados/Certificados.jsx
+++ src/components/Certificados/Certificados.jsx
@@ -1,15 +1,16 @@
 import "./Certificados.css";
 import { useLanguage } from "../../context/useLanguage.js";
+import certificadoReactTypescript from "../../assets/Certificado-CERT-1789601473793-2WAQ.pdf";
 
 function Certificados() {
     const { language, t } = useLanguage();
     const certificates = language === "pt" ? [
-        { title: "React com Typescript", institution: "Instituto DNC", workload: "12 horas", date: "15/09/2026", image: "/Certificados/react-typescript.svg", pdf: "/Certificados/react-typescript.pdf" },
+        { title: "React com Typescript", institution: "Instituto DNC", workload: "12 horas", date: "15/09/2026", image: "/Certificados/react-typescript.svg", pdf: certificadoReactTypescript },
         { title: "React", institution: "Instituto DNC", workload: "10 horas", date: "04/04/2026", image: "/Certificados/react.png", pdf: "/Certificados/react.pdf" },
         { title: "Javascript Avançado", institution: "Instituto DNC", workload: "8 horas", date: "04/01/2026", image: "/Certificados/javascript-avancado.png", pdf: "/Certificados/javascript-avancado.pdf" },
         { title: "HTML e CSS", institution: "Instituto DNC", workload: "10 horas", date: "03/08/2025", image: "/Certificados/html-css.png", pdf: "/Certificados/html-css.pdf" },
     ] : [
-        { title: "React with TypeScript", institution: "DNC Institute", workload: "12 hours", date: "15/09/2026", image: "/Certificados/react-typescript.svg", pdf: "/Certificados/react-typescript.pdf" },
+        { title: "React with TypeScript", institution: "DNC Institute", workload: "12 hours", date: "15/09/2026", image: "/Certificados/react-typescript.svg", pdf: certificadoReactTypescript },
         { title: "React", institution: "DNC Institute", workload: "10 hours", date: "04/04/2026", image: "/Certificados/react.png", pdf: "/Certificados/react.pdf" },
         { title: "Advanced JavaScript", institution: "DNC Institute", workload: "8 hours", date: "04/01/2026", image: "/Certificados/javascript-avancado.png", pdf: "/Certificados/javascript-avancado.pdf" },
         { title: "HTML and CSS", institution: "DNC Institute", workload: "10 hours", date: "03/08/2025", image: "/Certificados/html-css.png", pdf: "/Certificados/html-css.pdf" },
